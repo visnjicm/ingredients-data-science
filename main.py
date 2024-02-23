0eb5ef455e472b185a13a9b6134d25f694a456b2
Marko Visnjic <markovisnjic12@gmail.com>
2024-02-23 14:59:59 -0500

    sorted ingredients by frequency that they appear throughout recipes, created ingredient ranks list

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pickle
 import json
+
+import pandas
 import pandas as pd
 
 fp = 'archive/ingr_map.pkl'
@@ -13,7 +15,16 @@ fp = 'archive/PP_recipes.csv'
 tokens = pd.read_csv(fp)
 
 
-def filter_by_tag(tag: str):
+def check_tag(recipe_id: int, tag: str):
+    str_tag_list = recipes[recipes['id'] == recipe_id].tags.values[0]
+    tag_list = str_tag_list.strip("[]").replace("'", "").split(", ")
+    if tag in tag_list:
+        return True
+    else:
+        return False
+
+
+def get_recipes_by_tag(tag: str):
     return recipes[recipes['tags'].apply(lambda x: tag in x)]
 
 
@@ -29,7 +40,7 @@ def get_recipe_name(recipe_id: int):
     return recipes[recipes['id'] == recipe_id].name.values[0]
 
 
-def get_ingredients_names(ids_lst:list):
+def get_ingredients_names(ids_lst: list):
     lst = []
     for id in ids_lst:
         lst.append(get_ingredient_name(id))
@@ -43,7 +54,32 @@ recipes_ids = tokens.id.values
 
 ingredients_ids = tokens.ingredient_ids.values
 
-my_ingredients = get_ingredients_names(get_ingredients_ids(250063))
-my_recipe_name = get_recipe_name(250063)
+var4 = ingredients.sort_values(by='count', ascending=False).replaced.values
+
+ingredients_sorted = []
+
+for ingredient in var4:
+    if ingredient not in ingredients_sorted:
+        ingredients_sorted.append(ingredient)
+
+my_ingredients = get_ingredients_names(get_ingredients_ids(424415))
+my_recipe_name = get_recipe_name(424415)
+
+var = get_recipes_by_tag('main-dish')
+
+var3 = check_tag(424415, 'main-dish')
+
+for id in recipes_ids:
+    if check_tag(id, 'main-dish') and check_tag(id, 'healthy'):
+        my_ingredients = get_ingredients_names(get_ingredients_ids(id))
+        my_recipe_name = get_recipe_name(id)
+        my_ingredients_ids = get_ingredients_ids(id)
+        my_ingredients_ranks = []
+
+        for ingredient in my_ingredients:
+            var5 = ingredients[ingredients['processed'] == ingredient].replaced.values[0]
+            my_ingredients_ranks.append(ingredients_sorted.index(var5))
+            pass
+        pass
 
 pass
