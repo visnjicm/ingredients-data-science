0d52184613f3e1145eb7feabac10eb78c54d36b2
Marko Visnjic <markovisnjic12@gmail.com>
2024-02-26 12:14:49 -0500

    bug: seems to be an issue with the ingredient data missing some ingredients. fix: skip any recipe that has bad ingredient data

--- CookingSystem.py
+++ CookingSystem.py
@@ -1,63 +1,52 @@
 import json
 import pickle
+import ast
 
 import pandas as pd
 from matplotlib import pyplot as plt
 
 
 class CookingSystem:
+    # recipes_ids = get_recipes_by_tags(tags=('main-dish', 'healthy'))['id'].values
+    #
+    # ingredients_ids = tokens.ingredient_ids.values
+    #
+    # var4 = df_ingredients.sort_values(by='count', ascending=False).replaced.values
+    # my_ingredients = get_ingredients_names(get_ingredients_ids(424415))
+
+    # var = get_rec??ipes_by_tag('main-dish')
+    #
+    # var7 = get_recipes_by_tag('breakfast')
+    #
+    # var3 = check_tag(424415, 'main-dish')
+
+    # my_recipe_name = get_recipe_name(424415)
+
     fp = ['archive/ingr_map.pkl', 'archive/RAW_recipes.csv', 'archive/PP_recipes.csv']
 
-    def _ingr_map(self):
+    def __init__(self):
+        self.df_ingredients = self._df_ingr_map()
+        self.df_recipes = self._df_raw_recipes()
+        self.most_used_ingredients = self._most_used_ingredients()
+
+    @staticmethod
+    def df_to_csv(df:pd.DataFrame):
+        df.to_csv('ingredients.csv')
+
+
+    def _df_ingr_map(self):
         with open(self.fp[0], 'rb') as file:
-            ingredients = pickle.load(file)
-        return ingredients
+            return pickle.load(file)
 
-    def _raw_recipes(self):
-        recipes = pd.read_csv(self.fp[1])
-        return recipes
+    def _df_raw_recipes(self):
+        return pd.read_csv(self.fp[1])
 
-    def _tokens(self):
-        tokens = pd.read_csv(self.fp[2])
-        return tokens
+    def _df_tokens(self):
+        return pd.read_csv(self.fp[2])
 
-    def check_tag(self, recipe_id: int, tag: str):
-        recipes = self._raw_recipes()
-        str_tag_list = recipes[recipes['id'] == recipe_id].tags.values[0]
-        tag_list = str_tag_list.strip("[]").replace("'", "").split(", ")
-        if tag in tag_list:
-            return True
-        else:
-            return False
-
-    def get_recipes_by_tags(self, tags: tuple):
-        recipes = self._raw_recipes()
-        return recipes[recipes['tags'].apply(lambda x: all(tag in x for tag in tags))]
-
-    def get_ingredients_ids(self, recipe_id: int):
-        tokens = self._tokens()
-        var1 = tokens[tokens['id'] == recipe_id].ingredient_ids.values
-        var2 = var1[0]
-        var3 = json.loads(var2)
-        return var3
-
-    def get_ingredient_name(self, ingredient_id: int):
-        ingredients = self._ingr_map()
-        return ingredients[ingredients['id'] == ingredient_id].replaced.values[0]
-
-    def get_recipe_name(self, recipe_id: int):
-        recipes = self._raw_recipes()
-        return recipes[recipes['id'] == recipe_id].name.values[0]
-
-    def get_ingredients_names(self, ids_lst: list):
-        lst = []
-        for id in ids_lst:
-            lst.append(self.get_ingredient_name(id))
-        return lst
-
-    def get_most_used_ingredients(self):
+    def _most_used_ingredients(self):
         ingredients_sorted = []
-        ingredients = self._ingr_map()
+        ingredients = self._df_ingr_map()
         for ingredient in ingredients.sort_values(by='count', ascending=False).replaced.values:
             if ingredient not in ingredients_sorted:
                 ingredients_sorted.append(ingredient)
@@ -73,39 +62,96 @@ class CookingSystem:
         plt.grid(True)
         plt.show()
 
-    def plot_recipes_vs_ingredients_ranks(self, tags: tuple, stop: int):
+    def check_tag(self, recipe_id: int, tag: str):
+        str_tag_list = self.df_recipes[self.df_recipes['id'] == recipe_id].tags.values[0]
+        tag_list = str_tag_list.strip("[]").replace("'", "").split(", ")
+        if tag in tag_list:
+            return True
+        else:
+            return False
+
+    def get_recipes_by_tags(self, tags: tuple):
+        if tags is None:
+            return self.df_recipes
+        else:
+            return self.df_recipes[self.df_recipes['tags'].apply(lambda x: all(tag in x for tag in tags))]
+
+    def get_recipe_name(self, recipe_id: int):
+        return self.df_recipes[self.df_recipes['id'] == recipe_id].name.values[0]
+
+    def get_ingredients_ids(self, recipe_id: int) -> list:
+        recipe_ingredient_ids = []
+        var1 = self.df_recipes[self.df_recipes['id'] == recipe_id].ingredients.values
+        # print(self.get_recipe_name(recipe_id))
+        var2 = var1[0]
+        recipe_ingredients_names = ast.literal_eval(var2)
+        pass
+        for ingredient_name in recipe_ingredients_names:
+            df_row_match = self.df_ingredients[self.df_ingredients['raw_ingr'] == ingredient_name]
+            if df_row_match.empty:
+                return []
+            else:
+                ingredient_id = df_row_match.id.values[0]
+            recipe_ingredient_ids.append(ingredient_id)
+
+        pass
+        return recipe_ingredient_ids
+
+    def get_ingredient_name(self, ingredient_id: int):
+        return self.df_ingredients[self.df_ingredients['id'] == ingredient_id].replaced.values[0]
+
+    def get_ingredients_names(self, ids_lst: list = []):
+        lst = []
+        for id in ids_lst:
+            lst.append(self.get_ingredient_name(id))
+        return lst
+
+    def plot_recipes_vs_ingredients_ranks(self, tags:tuple = (), stop:int = -1):
         iterations = 0
-        recipes = self.get_recipes_by_tags(tags=tags)
-        recipes_ids = recipes['id'].values
-        lst_ingredients_ranks = []  # list of lists, the index of each element represents the iteration number
+        skipped = 0
+
+        self.df_recipes = self.get_recipes_by_tags(tags=tags)
+        self.recipes_ids = self.df_recipes['id'].values
+        self.lst_ingredients_ranks = []  # list of lists, the index of each element represents the iteration number
+
         x_values = []
         y_values = []
 
         # print(ingredients_sorted[0:200])
         # testing hypothesis that score/iterations will pretty much equal to 1, or very, very close to 1
-        for id in recipes_ids:
-            my_ingredients = self.get_ingredients_names(self.get_ingredients_ids(id))
-            my_ingredients_ranks = []
+        for id in self.recipes_ids:
+            recipe_ingredients_ids = self.get_ingredients_ids(id)
+            if len(recipe_ingredients_ids) == 0:
+                skipped += 1
+                continue
 
-            for ingredient in my_ingredients:
-                my_ingredients_ranks.append(self.get_most_used_ingredients().index(ingredient))
+            else:
+                my_ingredients = self.get_ingredients_names(recipe_ingredients_ids)
+                my_ingredients_ranks = []
 
-            lst_ingredients_ranks.append(my_ingredients_ranks)
+                for ingredient in my_ingredients:
+                    my_ingredients_ranks.append(self.most_used_ingredients.index(ingredient))
 
-            x_values = x_values + lst_ingredients_ranks[iterations]
-            y_values = y_values + ([iterations] * len(lst_ingredients_ranks[iterations]))
+                self.lst_ingredients_ranks.append(my_ingredients_ranks)
 
-            print()
-            print(f'Recipe Name: {self.get_recipe_name(id)}')
-            print(my_ingredients)
-            print(my_ingredients_ranks)
-            print()
+                x_values = x_values + self.lst_ingredients_ranks[iterations]
+                y_values = y_values + ([iterations] * len(self.lst_ingredients_ranks[iterations]))
 
-            iterations += 1
+                print()
+                print(f'Recipe Name: {self.get_recipe_name(id)}')
+                print(my_ingredients)
+                print(my_ingredients_ranks)
+                print(f'skipped: {skipped}')
 
-            if iterations == stop:
-                return self._plot_y_vs_x_scatter(x_values, y_values, 'x', 'y', 'my graph')
+                iterations += 1
+
+                if iterations == stop:
+                    return self._plot_y_vs_x_scatter(x_values, y_values, 'x', 'y', 'my graph')
 
         return self._plot_y_vs_x_scatter(x_values, y_values, 'x', 'y', 'my graph')
 
-    plot_recipes_vs_ingredients_ranks(('main-dish', 'healthy'), 5)
+
+if __name__ == "__main__":
+    my_cooking = CookingSystem()
+    my_cooking.df_to_csv(my_cooking.df_ingredients)
+    my_cooking.plot_recipes_vs_ingredients_ranks(('dessert'), 1000)
